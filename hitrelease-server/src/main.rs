e6d2d323d9aaa18d3167438f302046c166130b14
Max Crone <code@maxcrone.org>
2024-12-04 00:55:45 +0100

    Simplify server into song and about page

--- hitrelease-server/src/main.rs
+++ hitrelease-server/src/main.rs
@@ -1,22 +1,14 @@
-use askama::Template;
-use aws_config::BehaviorVersion;
-use aws_sdk_s3::{config::Region, presigning, Client as S3Client};
-use axum::{
-    extract::State,
-    handler::Handler,
-    http::StatusCode,
-    response::{Html, IntoResponse},
-    routing::{get, post},
-    Router,
-};
-use image;
-use qrcode::{render::svg, QrCode};
-use sqlx::SqlitePool;
-use std::{fs::File, io::Write, str::FromStr, sync::Arc};
+use axum::{extract::State, http::StatusCode, routing::get, Router};
+use hitrelease_util::Store;
+use std::{str::FromStr, sync::Arc};
 use uuid::Uuid;
 
 mod song;
-mod upload;
+
+// Template for the home/about page
+#[derive(askama::Template)]
+#[template(path = "index.html")]
+struct AboutTemplate {}
 
 // Template for the player page
 #[derive(askama::Template)]
@@ -27,10 +19,8 @@ struct PlayerTemplate {
 
 // Application state
 struct AppState {
-    db: SqlitePool,
-    s3_client: S3Client,
-    bucket_name: String,
-    // TODO: add tx handle to S3 uploading actor
+    endpoint: String,
+    song_cache: Store,
 }
 
 #[tokio::main]
@@ -38,35 +28,17 @@ async fn main() -> anyhow::Result<()> {
     // Initialize tracing
     tracing_subscriber::fmt::init();
 
-    // Initialize SQLite database
-    let db = SqlitePool::connect("sqlite:hitrelease.db?mode=rwc").await?;
-    sqlx::migrate!("./migrations").run(&db).await?;
-
-    let endpoint_url = std::env::var("S3_ENDPOINT").ok();
-
-    // Initialize S3 client
-    let mut config_loader =
-        aws_config::defaults(BehaviorVersion::latest()).region(Region::new("auto"));
-
-    if let Some(endpoint) = endpoint_url {
-        config_loader = config_loader.endpoint_url(endpoint);
-    }
-
-    let config = config_loader.load().await;
-    let s3_client = S3Client::new(&config);
-    let bucket_name = std::env::var("S3_BUCKET").expect("S3_BUCKET must be set");
+    let endpoint_url = std::env::var("S3_ENDPOINT").expect("should set the endpoint URL");
 
     // Create app state
     let state = Arc::new(AppState {
-        db,
-        s3_client,
-        bucket_name,
+        endpoint: endpoint_url,
+        song_cache: Store::default(),
     });
 
     // Build router
     let app = Router::new()
-        .route("/", get(upload::show_upload_page))
-        .route("/upload", post(upload::handle_upload))
+        .route("/", get(show_about))
         .route("/song/:id", get(show_player))
         .with_state(state);
 
@@ -76,6 +48,11 @@ async fn main() -> anyhow::Result<()> {
     axum::serve(listener, app).await?;
 
     Ok(())
+}
+
+// Handler for the home/about page
+async fn show_about(State(_): State<Arc<AppState>>) -> AboutTemplate {
+    AboutTemplate {}
 }
 
 // Handler for the player page
